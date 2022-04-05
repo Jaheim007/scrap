3fac1234e67421d620f43e951cec26954d7fb5a0
iMac-13 <imac-13@192.168.50.26>
2022-04-05 16:46:51 +0000

    have to put all in one condition

--- main1.py
+++ main1.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'https://just-scrape-it.com/collections/hoodie-sweat'
+url1 = 'https://just-scrape-it.com/collections/tshirt-t-shirt-tee-shirt'
+url2 = 'https://just-scrape-it.com/collections/gants'
+url3 = 'https://just-scrape-it.com/collections/maillots-ete'
+url4 = 'https://just-scrape-it.com/collections/stickers' 
 
 source = requests.get(url).text
 soup = BeautifulSoup(source, "lxml")
@@ -12,10 +16,34 @@ soup = BeautifulSoup(source, "lxml")
 # for il in price.find_all("li"):     
 #     print(il)
 
-price = soup.find('ul', class_ = "grid grid--uniform grid--view-items")
-deal = price.find()
+price = soup.find('ul', class_='grid grid--uniform grid--view-items')
+prix = soup.find_all('li', class_ = 'grid__item grid__item--collection-template small--one-half medium-up--one-quarter')
+map = soup.find_all('div', class_ = 'price__regular')
 
-print(price.text)
+for deals in prix:
+    print(deals.span.text)
+
+for m in map:       
+    print(m.dd.text)
+
+for d in prix:     
+    print(d.img["src"])
+
+    
+    # print(first_item)
+       
+  
+
+       
+       
+    
+    
+
+
+
+# print(price.text)
+# print(deal)
+# print(prix.text)
 
 # for i in soup.find('ul', class_='grid grid--uniform grid--view-items'):        
 #     if price.find('span', class_='price-item price-item--regular') == "Épuisé":     
